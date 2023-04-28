c09e525c3a469830a1188be28e8728c0e4385eff
shalash96 <m_shalash96@yahoo.com>
2023-04-28 19:12:17 +0100

    fixing some typos

--- ADVFAN.py
+++ ADVFAN.py
@@ -1,10 +1,10 @@
-from SCRIPTS.LigandPrepartion import ligandPrearation
+from SCRIPTS.LigandPrepartion import ligandPreparation
 from SCRIPTS.ReceptorPreparation import receptorPreparation
 from SCRIPTS.CreatingFolders import creatingFolders
 from SCRIPTS.VinaBoxCreation import vinaBoxCreation
 
 if __name__ == "__main__":
     creatingFolders()
-    ligandPrearation()
+    ligandPreparation()
     receptorPreparation()
     vinaBoxCreation()
